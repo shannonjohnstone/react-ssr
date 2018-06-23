bf105a444e0855ba53b476584fbb9751ab1ed4c9
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-23 22:17:14 +1000

    clean imports

--- server/webpack/webpack.server.js
+++ server/webpack/webpack.server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base.js');
 const webpackNodeExternals = require('webpack-node-externals');
+
+const baseConfig = require('./webpack.base.js');
 
 const config = {
   target: 'node',
